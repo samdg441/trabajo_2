71db7dee21b26d5a64907485db1a59cca29dab8f
samdg441 <samuelgallego441@gmail.com>
2024-02-14 18:04:43 -0500

    finish exercise 1

---
+++ ejercicios repaso/Ejercicio 1.py
@@ -0,0 +1,8 @@
+#1. Crear un programa que pida al usuario ingresar su nombre y edad y los imprima en pantalla
+
+nombre= (input("ingrese su nombre "))
+
+edad = int(input("ingrese su edad "))
+
+print("nombre=", nombre, "edad=", edad)
+
