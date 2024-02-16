5c0d656116935daa1b94e4cf89531e46a8a48c1f
jpenuela420 <jpenuela560@soyudemedellin.edu.co>
2024-02-15 19:43:05 -0500

    ejercicio determinar si un número dado es par o impar

---
+++ ejercicios repaso/Ejercicio_4.py
@@ -0,0 +1,7 @@
+def main():
+    numero = int(input("Ingrese un número:"))
+    if numero % 2 == 0:
+        print(f"El número {numero} es par")
+    else:
+        print(f"El número {numero} es impar")
+main()
